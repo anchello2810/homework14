2c847b4bf0330e5450ddaec7f6af8a3e4681fc81
Anna Akopyan <anchello@yandex.ru>
2021-05-25 17:47:54 +0300

    Добавьте файлы проекта.

---
+++ homework14.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+#include <string>
+
+int main()
+{
+    
+    std::string phrase = "The London is capital of Great Britain";
+    std::cout << phrase << "\n";
+    std::cout << phrase.length() << "\n";
+    std::cout << phrase[0] << std::endl;
+    std::cout << phrase[37] << std::endl;
+    return 0;
+
+}
+
